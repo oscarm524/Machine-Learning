d902d8f47fc2cd612a53561ff7a39fa6e4a61b29
oscarm524 <oscar.aguilar.1987@gmail.com>
2020-05-05 13:24:07 -0500

    Updating R-script

--- Standard_Models/Random_Forest_Regressor.R
+++ Standard_Models/Random_Forest_Regressor.R
@@ -2,3 +2,17 @@
 ## Random Forest Regressor ##
 #############################
 
+Random_Forest_Regressor <- function(X, Y){
+   
+   ## Checking for randomforest package
+   if (!require(randomForest, character.only = T, quietly = T)) {
+      install.packages(randomForest)
+      library(randomForest, character.only = T)
+   }
+   
+   ## This function assumes that X is the data.frame/matrix of
+   ## input and Y is the target varible
+   
+   
+   
+}
